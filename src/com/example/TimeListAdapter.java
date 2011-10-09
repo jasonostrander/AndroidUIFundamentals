92ae9192f7fd0b39541bc6baf09c20d3d3a828f9
Jason Ostrander <jason.ostrander@gmail.com>
2011-10-09 14:22:01 -0700

    Simplify the adapter.

--- src/com/example/TimeListAdapter.java
+++ src/com/example/TimeListAdapter.java
@@ -1,8 +1,5 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,21 +9,8 @@ import android.widget.TextView;
 
 public class TimeListAdapter extends ArrayAdapter<Time> {
     
-    private List<Time> times = new ArrayList<Time>();
-    
     public TimeListAdapter(Context context, int textViewResourceId) {
         super(context, textViewResourceId);
-    }
-    
-    @Override
-    public void add(Time object) {
-        times.add(object);
-        super.add(object);
-    }
-    
-    @Override
-    public int getCount() {
-        return times.size();
     }
     
     @Override
@@ -36,10 +20,10 @@ public class TimeListAdapter extends ArrayAdapter<Time> {
             view = LayoutInflater.from(getContext()).inflate(R.layout.time_row, null);
         }
         
-        Time time = times.get(position);
+        Time time = getItem(position);
         
         TextView name = (TextView) view.findViewById(R.id.lap_name);
-        name.setText(time.name);
+        name.setText("Session " + (position+1) );
         
         TextView lapTime = (TextView) view.findViewById(R.id.lap_time);
         lapTime.setText(time.time);
