e7fc9c46a7a74fac9e6781c33610e45995aa06e2
Jason Ostrander <jason.ostrander@gmail.com>
2011-12-03 19:18:17 -0800

    Return proper views.

--- Chapter5/src/com/example/TaskListFragment.java
+++ Chapter5/src/com/example/TaskListFragment.java
@@ -2,10 +2,14 @@ package com.example;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class TaskListFragment extends ListFragment {
     @Override
-    public void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.task_list, container);
     }
 }
--- Chapter5/src/com/example/TimerFragment.java
+++ Chapter5/src/com/example/TimerFragment.java
@@ -15,8 +15,7 @@ public class TimerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.task_detail, container);
-        return view;
+        return inflater.inflate(R.layout.task_detail, container);
     }
 
     
