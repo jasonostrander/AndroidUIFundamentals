e7525c04fbde2330b1dd33637bac60cd99bf6c81
Jason Ostrander <jason.ostrander@gmail.com>
2012-02-18 16:20:43 -0800

    Clean up imports.

--- Chapter10/CustomViewsExample/src/com/example/CrossView.java
+++ Chapter10/CustomViewsExample/src/com/example/CrossView.java
@@ -5,9 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 
--- Chapter10/CustomViewsExample/src/com/example/CustomViewsActivity.java
+++ Chapter10/CustomViewsExample/src/com/example/CustomViewsActivity.java
@@ -2,7 +2,6 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.CheckBox;
 
 public class CustomViewsActivity extends Activity {
     @Override
