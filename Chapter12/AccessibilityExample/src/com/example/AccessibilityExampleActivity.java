55fe10bb7656e5f31fb17567b6596e04dd8565ac
Jason Ostrander <jason.ostrander@gmail.com>
2012-02-18 16:05:37 -0800

    Clean up import, code.

--- Chapter12/AccessibilityExample/src/com/example/AccessibilityExampleActivity.java
+++ Chapter12/AccessibilityExample/src/com/example/AccessibilityExampleActivity.java
@@ -2,15 +2,12 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class AccessibilityExampleActivity extends Activity {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main);
         setContentView(R.layout.focus_example);
-        
-        getResources().getString(R.string.hello);
     }
 }
